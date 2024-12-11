1f3aa884f80b7354b59b742aca4d341118b6daab
Marti <martyagusan@gmail.com>
2024-12-11 16:04:24 +0100

    Teacher base functions

--- Teacher.py
+++ Teacher.py
@@ -1,11 +1,16 @@
-
+from Human import Human
+from Toddler import Toddler
 from Game import Game
 
 
-class Teacher:
+class Teacher(Human):
     def __init__(self, position, direction):
-        self.position = position
+        super().__init__(position)
         self.direction = direction
+
+    def distance(self, other) -> float:
+        #Return the Manhattan distance between the current agent and another agent.
+        return abs(self.position[0] - other.position[0]) + abs(self.position[1] - other.position[1])
 
 
     def watch_children(self):
@@ -40,27 +45,3 @@ class Teacher:
                 self.move_down()
 
 
-    def distance(self, other) -> float:
-        #Return the Manhattan distance between the current agent and another agent.
-        return abs(self.position[0] - other.position[0]) + abs(self.position[1] - other.position[1])
-
-
-    def get_position(self) -> tuple:
-        return self.position
-
-    def set_position(self, position: tuple):
-        self.position = position
-
-    def move_up(self):
-        self.position = (self.position[0], self.position[1] + 1)
-
-    def move_down(self):
-        self.position = (self.position[0], self.position[1] - 1)
-
-    def move_left(self):
-        self.position = (self.position[0] - 1, self.position[1])
-        self.direction="left"
-
-    def move_right(self):
-        self.position = (self.position[0] + 1, self.position[1])
-        self.direction="right"
